8ae56af859e14dc477fcef1e07e1c21ac79c0983
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2026-05-04 00:17:18 +0000

    add file

---
+++ express-05/api/models/pessoas.js
@@ -0,0 +1,41 @@
+const getPessoaModel = (sequelize, Sequelize) => {
+  const Pessoa = sequelize.define(
+    "Pessoa",
+    {
+      id: {
+        type: Sequelize.DataTypes.UUID,
+        defaultValue: Sequelize.DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      nome: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      resumo: {
+        type: Sequelize.DataTypes.TEXT,
+      },
+    },
+    {
+      tableName: "pessoas",
+      timestamps: true,
+    }
+  );
+
+  
+  Pessoa.associate = (models) => {
+    Pessoa.hasMany(models.Experiencia, { 
+      foreignKey: "pessoaId", 
+      as: "experiencias" 
+    });
+  };
+
+  return Pessoa;
+};
+
+module.exports = getPessoaModel;
