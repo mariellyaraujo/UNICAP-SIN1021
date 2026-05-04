65f729fb5c5aece3acf7691182511c0cd4303f93
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2026-05-04 01:25:10 +0000

    fix

--- express-05/api/routes/index.js
+++ express-05/api/routes/index.js
@@ -5,12 +5,20 @@ const pessoaController = require("../controllers/pessoaController");
 const experienciaController = require("../controllers/experienciaController");
 const formacaoController = require("../controllers/formacaoController");
 
-router.post("/pessoas", pessoaController.criar);
 router.get("/pessoas", pessoaController.listar);
 router.get("/pessoas/:id", pessoaController.buscarPorId);
+router.post("/pessoas", pessoaController.criar);
+router.put("/pessoas/:id", pessoaController.atualizar);
 router.delete("/pessoas/:id", pessoaController.excluir);
 
+router.get("/experiencias", experienciaController.listar);
 router.post("/experiencias", experienciaController.criar);
+router.put("/experiencias/:id", experienciaController.atualizar);
+router.delete("/experiencias/:id", experienciaController.excluir);
+
+router.get("/formacoes", formacaoController.listar);
 router.post("/formacoes", formacaoController.criar);
+router.put("/formacoes/:id", formacaoController.atualizar);
+router.delete("/formacoes/:id", formacaoController.excluir);
 
 module.exports = router;
