37a7a7559b77766cfd87ba6af5df2aadfef1575a
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2026-05-06 02:11:56 +0000

    fix add instalando o cors pra integrar com o front

--- express-05/api/index.js
+++ express-05/api/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const db = require("./models"); 
 const routes = require("./routes"); 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", routes); 
